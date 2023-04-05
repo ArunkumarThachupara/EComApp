d281a5a62ecc5b2b033977e3f6065588fbbf92ad
Arunkumar T R <arunkumar.tr@outlook.com>
2023-04-05 23:17:32 +0530

    initial commit

---
+++ app/src/main/java/com/zen4r17/loginapplication2/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.zen4r17.loginapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Add any necessary initialization code here
+
+        // Set the content view to the splash screen layout
+        setContentView(R.layout.activity_splash);
+
+        // Use a Handler to launch the main activity after a certain delay
+        new
+                Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // Wait for 3 seconds before launching the main activity
+    }
+}
+
+
+
+
+//public class SplashActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+//    }
+//}
